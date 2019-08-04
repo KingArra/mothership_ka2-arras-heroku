ed87b0703546b181f7d774ff463caa756d2b014b
Glitch (arras-domination-test) <none>
2019-08-04 13:13:55 +0000

    🚡🏇 Checkpoint ./server.js:2436677/115

--- server.js
+++ server.js
@@ -4684,15 +4684,15 @@ var maintainloop = (() => {
     // The NPC function
     let makenpcs = (() => {
         // Make base protectors if needed.
-            /*let f = (loc, team) => { 
+            let baseProtectors = (loc, team) => { 
                 let o = new Entity(loc);
                     o.define(Class.baseProtector);
                     o.team = -team;
                     o.color = [10, 11, 12, 15][team-1];
             };
-            for (let i=1; i<5; i++) {
-                room['bas' + i].forEach((loc) => { f(loc, i); }); 
-            }*/
+            for (let i=1
+                room['bas' + i].forEach((loc) => { baseProtectors(loc, i); }); 
+            }
         // Return the spawning function
         let bots = [];
         return () => {
