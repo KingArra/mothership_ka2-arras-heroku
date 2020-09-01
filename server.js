27894372a3da3ae96b50411a96f30f9197f6077a
Glitch (arras-domination-test) <none>
2020-09-01 20:08:09 +0000

    ⛷💁 Checkpoint ./server.js:13763590/14165

--- server.js
+++ server.js
@@ -682,20 +682,19 @@ class io_minion extends IO {
         }
     }
 }
-ioTypes.hangOutNearMaster = class extends IO {
+class io_hangOutNearMaster extends IO {
     constructor(body) {
-        super(body)
-        this.acceptsFromTop = false
-        this.orbit = 30
-        this.currentGoal = { x: this.body.source.x, y: this.body.source.y, }
-        this.timer = 0
+        super(body);
+        this.acceptsFromTop = false;
+        this.orbit = 30;
+        this.currentGoal = { x: this.body.source.x, y: this.body.source.y, };
+        this.timer = 0;
     }
     think(input) {
-        if (this.body.invisible[1]) return {}
-        if (this.body.source !== this.body) {
-            let bound1 = this.orbit * 0.8 + this.body.source.size + this.body.size
-            let bound2 = this.orbit * 1.5 + this.body.source.size + this.body.size
-            let dist = util.getDistance(this.body, this.body.source) + Math.PI / 8;
+        if (this.body.source != this.body) {
+            let bound1 = this.orbit * 0.8 + this.body.source.size + this.body.size;
+            let bound2 = this.orbit * 1.5 + this.body.source.size + this.body.size;
+            let dist = util.getDistance(this.body, this.body.source) + Math.PI / 8; 
             let output = {
                 target: {
                     x: this.body.velocity.x,
@@ -703,7 +702,7 @@ ioTypes.hangOutNearMaster = class extends IO {
                 },
                 goal: this.currentGoal,
                 power: undefined,
-            };
+           };
             // Set a goal
             if (dist > bound2 || this.timer > 30) {
                 this.timer = 0
