69f5285d9f3184e1365e432a0b36d9012bc4690b
Glitch (d-dayex) <none>
2020-09-30 15:47:56 +0000

    🕵🚕 Checkpoint ./server.js:22550751/4 ./config.json:22550751/42

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ const room = {
     room.findType('bmaz');
     room.findType('domi');
     room.findType('dom1');
-    room.findType('dom3');
+    room.findType('dom0');
     room.findType('roid');
     room.findType('rock');
     room.nestFoodAmount = 1.5 * Math.sqrt(room.nest.length) / room.xgrid / room.ygrid;
@@ -4690,7 +4690,7 @@ var maintainloop = (() => {
   
   if (room.gameMode === 'tdm') room['domi'].forEach((loc) => { createDom(loc, -1, ran.choose([ Class.gunnerDominator, Class.destroyerDominator,])); }); 
   if (room.gameMode === 'tdm') room['dom1'].forEach((loc) => { createDom(loc, -1, ran.choose([ Class.gunnerDominator, Class.destroyerDominator,])); }); 
-  if (room.gameMode === 'tdm') room['dom3'].forEach((loc) => { createDom(loc, -3, ran.choose([ Class.gunnerDominator, Class.destroyerDominator,])); });     placeRoids();
+  if (room.gameMode === 'tdm') room['dom0'].forEach((loc) => { createDom(loc, -3, ran.choose([ Class.gunnerDominator, Class.destroyerDominator,])); });     placeRoids();
     // Spawning functions
     let spawnBosses = (() => {
         let timer = 0;
